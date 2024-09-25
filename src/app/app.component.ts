2a6b4eac75e0efaa9f836be9d52768ea465e9b20
Franco Ceballos <francomanuelceballos@gmail.com>
2024-09-25 12:01:54 -0300

    fetched products from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  title = 'Store App';
+  constructor(private http:HttpClient){}
+  products:any[]=[];
+  ngOnInit(): void {
+    this.http.get("https://localhost:7125/api/product?pageSize=50").subscribe({
+      next : (response:any) => this.products=response.data,
+      error:error => console.log(error),
+      complete: ()=>{
+        console.log("Resquest completed")
+        console.log("Extra statements")
+      }
+    })
+  }
 }
